a8b9aeca0fc5b79ab4250d5702b343f3e548902a
Martin <jmlarreyna@gmail.com>
2022-07-24 17:08:12 -0300

    Responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,7 +19,6 @@ const NavBar = () => {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
             navbarScroll
           >
             <Nav.Link className="inicial" href="/">Home</Nav.Link>
