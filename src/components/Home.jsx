ca150e8632e0b0ef66eef62bf62d7ce7344ec81a
Martin <jmlarreyna@gmail.com>
2022-07-25 18:52:10 -0300

    deploy-final

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,57 +42,6 @@ return(
   </div>
 
 
-      /* <div className="especial" >
-        <Carousel>
-          <Carousel.Item interval={3000} controls="false">
-            <img
-              className="d-block w-100"
-              src={refugio}
-            />
-
-          </Carousel.Item>
-
-          <Carousel.Item interval={3000}>
-            <img
-              className="d-block w-100"
-              src={refugio}
-            />
-          
-          </Carousel.Item>
-
-          <Carousel.Item interval={3000} > 
-            <img
-              className="d-block w-100"
-              src={refugio}
-            />
-
-          </Carousel.Item>
-        </Carousel>
-    </div>
-
-    <div className="flex-container2">
-      <div className="especial">
-        <h1>
-          ¡Bienvenidos a ADOPTANDO!
-          </h1>
-        
-        <p>
-          Somos una organización sin fines de lucro que busca darle un hogar a todos los perritos que la comunidad nos acerca. Nuestra propuesta es muy sencilla,
-          si pensas que podés ser un buen candidato para alguno de ellos, llená el formulario y te contactaremos para coordinar una visita.
-        </p>
-
-        <p>
-        ¡Hacé click más abajo y conocé a todas las patitas que están esperando ser adoptadas!
-        </p>
-
-        <Button className='normal' style={{ width: '50%'  }} size="sm" href="/mascotas" >EMPEZAR!</Button>
-
-
-
-      </div> 
-  
-    </div>*/
-
 )
 }
 
